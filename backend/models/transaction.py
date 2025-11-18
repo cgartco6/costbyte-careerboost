2821950aa8e06369c46548ccea52268d1370483b
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-11-18 11:59:25 +0200

    Create transaction.py

---
+++ backend/models/transaction.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey
+from backend.database import Base
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    amount = Column(Float)
